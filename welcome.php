b65161bbc67cd56e9fd88ab58c20b03f965608cd
Alex <alex11737@gmail.com>
2021-01-06 20:38:45 +0200

    fixed small bug

--- welcome.php
+++ welcome.php
@@ -114,7 +114,7 @@ else
         let nor = document.getElementById("loader");
         nor.style.display = "none";
         var x = document.getElementById("content");
-        x.style.display = "block";
+        x.style.visibility = "visible";
         }, 1400);
         </script>
     </body>
