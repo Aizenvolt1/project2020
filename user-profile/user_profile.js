f12feb3820715722b7dd91b0e552bc275e4ca3e2
Alex <alex11737@gmail.com>
2021-03-08 20:26:29 +0200

    finished task 4 from user

--- user-profile/user_profile.js
+++ user-profile/user_profile.js
@@ -11,7 +11,6 @@ room.addEventListener("click", (e) => {
   });
 });
 
-let coordinates = [2];
 function set_coordinates(data_from_server) {
   return new Promise((resolve, reject) => {
     $.ajax({
@@ -128,14 +127,19 @@ function password_check() {
 }
 // Creating map options
 async function make_map() {
+  let coordinates = [2];
   let data_from_server;
   let max_count;
   let map_data = await set_coordinates(data_from_server);
   for (let i = 0; i < map_data.length; i++) {
     if (i === 0) {
       max_count = map_data[i].count;
+      coordinates[0] = map_data[i].lat;
+      coordinates[1] = map_data[i].lng;
     } else if (i > 0 && map_data[i].count > max_count) {
       max_count = map_data[i].count;
+      coordinates[0] = map_data[i].lat;
+      coordinates[1] = map_data[i].lng;
     }
   }
   // Creating a map object
@@ -151,14 +155,8 @@ async function make_map() {
   map.addLayer(layer);
 
   let testData = {
-    max: 3,
-    data: [
-      { lat: 38.246242, lng: 21.735085, count: 3 },
-      { lat: 38.323343, lng: 21.865082, count: 2 },
-      { lat: 38.34381, lng: 21.57074, count: 8 },
-      { lat: 38.108628, lng: 21.502075, count: 7 },
-      { lat: 38.123034, lng: 21.917725, count: 4 },
-    ],
+    max: max_count,
+    data: map_data,
   };
   let cfg = {
     radius: 40,
