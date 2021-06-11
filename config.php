b0370f14f85fe02a31197f7bcd25af9586a3850f
Alex <alex11737@gmail.com>
2021-06-11 17:10:56 +0300

    small change in comments

--- config.php
+++ config.php
@@ -1,15 +1,13 @@
 
 <?php
 
- /* at the top of 'check.php' */
     if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
         /* 
-           Up to you which header to send, some prefer 404 even if 
-           the files does exist for security
+           The header to send
         */
         header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
 
-        /* choose the appropriate page to redirect users */
+        /*Here we choose the page to redirect users */
         die( header( 'location: welcome.php' ) );
 
     }
